fb41227ec12d1741c224c58d004980bd3a85834b
ebukaamadiobi <ebukaamadiobi@gmail.com>
2025-11-07 20:54:22 +0000

    added details

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,7 @@
 import logo from "/logo.svg"
 import { useState, useEffect, useRef } from 'react'
+import { AiFillGithub, AiFillLinkedin, AiFillInstagram, AiFillX } from "react-icons/ai";
+import { FaXTwitter } from "react-icons/fa6";
 
 // About
 // Experience
@@ -46,25 +48,33 @@ function Header() {
 export default function Home() {
     
     return (
-      <div className="flex flex-col">
+      <div className="w-screen flex flex-col bg-[#1f0e07]">
         <Header/>
-        <div className='w-full flex flex-col md:flex-row bg-[#1f0e07]'>
-          <div className='h-[calc(100vh-10rem)] md:w-[60vw] flex items-center mx-10 md:mx-15 lg:mx-32'>
+        <div className='flex flex-col md:flex-row'>
+          <div className='h-[60vh] md:h-[calc(100vh-7.5rem)] md:w-[60vw] flex items-center mx-10 md:mx-15 lg:mx-32 relative'>
             <div>
               <h1>Hi, I'm</h1>
-              <h3 className='text-[#8b3923] font-semibold text-[5rem] md:text-[11rem] tracking-[-0.06em] leading-24 md:leading-40'>
+              <h3 className='text-[#8b3923] font-semibold text-[5rem] md:text-[7rem] lg:text-[11rem] tracking-[-0.06em] leading-24 md:leading-30 lg:leading-40'>
                 Ebuka.
               </h3>
               <h1>I build stuff with computers.</h1>
               <br/>
               <div className="md:text-2xl text-[#ab795e]">I'm a recent graduate of electronic engineering from UCD, 
-now I'm working my way into the software industry.
-Currently, working on streamlining the CV tailoring 
-process with <a href="https://resumate.cv" target="_blank">Resumate.</a></div>
+                  now I'm working my way into the software industry.
+                  Currently, working on streamlining the CV tailoring 
+                  process with <a href="https://resumate.cv" target="_blank">Resumate.</a>
+              </div>
+              <div className="flex flex-row gap-3 items-center">
+                <a href="https://github.com/EbukaAmadiObi" target="_blank"><AiFillGithub className="text-[#cd9272] size-7 md:size-11"/></a>
+                <a href="https://www.linkedin.com/in/ebuka-amadi-obi/" target="_blank"><AiFillLinkedin size={45} className="text-[#cd9272] size-7 md:size-11"/></a>
+                <a href="https://www.instagram.com/ebuka_amadi_obi/" target="_blank"><AiFillInstagram size={45} className="text-[#cd9272] size-7 md:size-11"/></a>
+                <a href="https://x.com/ebuka_dot_exe" target="_blank"><FaXTwitter size={45} className="text-[#cd9272] size-7 md:size-11"/></a>
+                <div className="mx-5 md:mx-10 my-10 border-b-3 opacity-25 w-full h-1"/>
+              </div>
             </div>
           </div>
-          {/*<img src={hero} className="ml-auto md:w-[40vw] h-auto object-contain" alt="Letter pattern" />*/}
-          <picture>
+          <div className="ml-auto w-screen md:w-[40vw] md:h-[calc(100vh-7.5rem)] overflow-hidden relative">
+            <picture>
               <source
                 media="(min-width:768px)"
                 type="image/webp"
@@ -83,8 +93,11 @@ process with <a href="https://resumate.cv" target="_blank">Resumate.</a></div>
                 loading="lazy"
               />
             </picture>
+            {/* Bottom gradient overlay to soften image edge */}
+            <div className="pointer-events-none absolute left-0 bottom-0 w-full h-24 md:h-32 bg-[linear-gradient(to_bottom,#1f0e0700,#1f0e07ff)]" />
+          </div>
         </div>
-
+        <div className="mx-30 my-10 border-b-2 opacity-25"/>
       </div>
     )
 };
