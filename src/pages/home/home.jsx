65f2e44a2f00b19ef71a6f8c3a08f677605111b7
ebukaamadiobi <ebukaamadiobi@gmail.com>
2026-01-14 19:09:53 +0000

    added extra info and removed experience formatting

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -7,7 +7,6 @@ import {
 } from "../../components/ui/hover-card"
 import { AiFillGithub, AiFillLinkedin, AiFillInstagram, AiFillX } from "react-icons/ai";
 import { FaXTwitter } from "react-icons/fa6";
-import Experience from "./experience";
 
 export default function Home() {
     
@@ -78,18 +77,68 @@ export default function Home() {
             </p>
             <br/>
             <p>
-              A recent graduate of electronic engineering from UCD, I'm now working on doing 
-              cool stuff with software. Currently, I'm building an app to streamline the CV 
-              tailoring process with <a href="https://resumate.cv" target="_blank">Resumate.</a>
+              I'm currently working on building community and engaging with startup ecosystems 
+              worldwide as a Special Projects Associate at <a href="https://websummit.com" target="_blank">Web Summit</a>.
+            </p>
+            <br/>
+            <p>
+              I hold a BEng. in Electronic Engineering from UCD. My bachelor's thesis was titled: 
+              "Edge AI as a Service (EAIaaS) Platform for Launching AI/ML Services." 
+            </p>
+            <br/>
+            <p>
+              I was a Software Engineering Intern at Arista Networks (May 2024), working on their
+              cloud management platform to improve user experience and functionality. (I also learned
+              Go, great fun.)
+            </p>
+            <br/>
+            <p>
+              Last year, I did the{" "}
+              <TouchHoverCard trigger="Production Engineering" triggerClassName="text-[#ab795e]">
+                <HoverCardContent className="w-80 bg-[#2a1308] border-[#3e1e0f]">
+                  <div className="flex justify-between gap-4">
+                    Honestly one of the most productive summers I've had so far, PE is such an important
+                    field for anyone interested in indie hacking or running SaaS products.
+                  </div>
+                </HoverCardContent>
+              </TouchHoverCard> Fellowship with Meta & MLH (Jun 2025), where I worked 
+              with a CentOS VPS, and containerized services.
+            </p>
+            <br/>
+            <p>
+              In my free time, I enjoy building projects.
+              Currently, I'm building an app to streamline the CV 
+              tailoring process. It's called <a href="https://resumate.cv" target="_blank">Resumate.</a>
             </p>
             <br/>
             <p>
               You're likely to find me at one of the few tech meetups in Dublin, including the ones 
-              I help organise at <a href="https://giveago.co/" target="_blank">Give(a)Go</a>
+              I help organise at <a href="https://giveago.co/" target="_blank">Give(a)Go</a>, and the
+               ones I started at {" "} <a href="https://dockyard.info/" target="_blank">Dockyard</a>
             </p>
-            <p>
-              
+            <br/>
+            <p className="font-bold pb-1">
+              Some other cool things I've done:
             </p>
+            <ul className="list-disc space-y-1 px-6 list-outside">
+              <li>I was one of 52 student countrywide to be selected as a {" "}
+                <TouchHoverCard trigger="Web Summit Scholar" triggerClassName="text-[#ab795e]">
+                <HoverCardContent className="w-80 bg-[#2a1308] border-[#3e1e0f]">
+                    <p>Great experience, got to attend Web Summit 2025 in Lisbon, meet loads of
+                    interesting people and it's what led to my current role.</p> <br/> Check it out <a href="https://websummit.com/scholarship-programme/" target="_blank">here.</a>
+                </HoverCardContent>
+              </TouchHoverCard></li>
+              <li>I was also one of 20 students selected worldwide to attend {" "}<TouchHoverCard trigger="Invisible College" triggerClassName="text-[#ab795e]">
+                <HoverCardContent className="w-80 bg-[#2a1308] border-[#3e1e0f]">
+                  <p>Spent a week discussing topics like urbanism, the industrial revolution and metascience.</p> <br/> <p>Surrounded by insanely intelligent and articulate people, felt very stupid.</p> <br/>Check it out <a href="https://www.worksinprogress.news/p/apply-for-invisible-college-2025" target="_blank">here.</a>
+                </HoverCardContent>
+              </TouchHoverCard> in Cambridge, hosted by Works in Progress</li>
+              <li>I won third in the Dublin Google Hackathon in 2024 and was the regional winner for the {" "}<TouchHoverCard trigger="National AI Challenge 2024" triggerClassName="text-[#ab795e]">
+                <HoverCardContent className="w-80 bg-[#2a1308] border-[#3e1e0f]">
+                    Won me three months of membership to Dogpatch Labs, you'd better believe I made the most of that.
+                </HoverCardContent>
+              </TouchHoverCard></li>
+            </ul>
           </div>
           <img
             src="/images/duotone (1).png"
@@ -98,7 +147,7 @@ export default function Home() {
             loading="lazy"
           />
         </div>
-        <Experience/>
+        <div className="colsection"></div>
       </div>
     )
 };
